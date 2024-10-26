84a40a19593f4a87ed8cc19113590be3de8d41d5
Lukasz08 <sonerr55555@gmail.com>
2024-10-26 16:01:53 +0200

    Update kolkoikrzyzyk.py

--- kolkoikrzyzyk.py
+++ kolkoikrzyzyk.py
@@ -1,5 +1,5 @@
-P = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-
+P = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spr = 0
 def ruch(P):
     for k in P:
         print(k)
@@ -7,8 +7,24 @@ def ruch(P):
 def ruch_x(P):
     a = int(input("podaj wiersz:"))
     b = int(input("podaj kolumnę:"))
-    P.pop(a[b])
-    P.insert(a[b], x)
+    P[a][b] = 1
+    for k in P:
+        print(k)
 
-for i in range(9):
+def ruch_o(P):
+    a = int(input("podaj wiersz:"))
+    b = int(input("podaj kolumnę:"))
+    P[a][b] = 2
+    for k in P:
+        print(k)
+
+while spr == 0:
     ruch_x(P)
+    if P[0][0] == 2 and P[0][1] == 2 and P[0][2] == 1 or P[1][0] == 2 and P[1][1] == 2 and P[1][2] == 2 or P[2][0] == 2 and P[2][1] == 2 and P[2][2] == 2 or P[0][0] == 2 and P[1][0] == 2 and P[2][0] == 2 or P[0][1] == 2 and P[1][1] == 2 and P[2][1] == 2 or P[0][2] == 2 and P[1][2] == 2 and P[2][2] == 2 or P[0][0] == 2 and P[1][1] == 2 and P[2][2] == 2 or P[0][2] == 2 and P[1][1] == 2 and P[2][0] == 2:
+        spr = 1
+        print("wygrywa kółko")
+    ruch_o(P)
+    if P[0][0] == 1 and P[0][1] == 1 and P[0][2] == 1 or P[1][0] == 1 and P[1][1] == 1 and P[1][2] == 1 or P[2][0] == 1 and P[2][1] == 1 and P[2][2] == 1 or P[0][0] == 1 and P[1][0] == 1 and P[2][0] == 1 or P[0][1] == 1 and P[1][1] == 1 and P[2][1] == 1 or P[0][2] == 1 and P[1][2] == 1 and P[2][2] == 1 or P[0][0] == 1 and P[1][1] == 1 and P[2][2] == 1 or P[0][2] == 1 and P[1][1] == 1 and P[2][0] == 1:
+        spr = 1
+        print("wygrywa krzyżyk")
+
