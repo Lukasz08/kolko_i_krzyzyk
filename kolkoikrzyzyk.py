41780d0a8ce3692f613683665f00a008ffb81e60
Lukasz08 <sonerr55555@gmail.com>
2024-11-02 12:04:47 +0100

    Update kolkoikrzyzyk.py

--- kolkoikrzyzyk.py
+++ kolkoikrzyzyk.py
@@ -1,30 +1,33 @@
-P = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+P = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 spr = 0
-def ruch(P):
-    for k in P:
-        print(k)
 
 def ruch_x(P):
     a = int(input("podaj wiersz:"))
     b = int(input("podaj kolumnę:"))
-    P[a][b] = 1
+    while P[a][b] != ' ':
+        a = int(input("podaj wiersz:"))
+        b = int(input("podaj kolumnę:"))
+    P[a][b] = "x"
     for k in P:
         print(k)
 
 def ruch_o(P):
     a = int(input("podaj wiersz:"))
     b = int(input("podaj kolumnę:"))
-    P[a][b] = 2
+    while P[a][b] != ' ':
+        a = int(input("podaj wiersz:"))
+        b = int(input("podaj kolumnę:"))
+    P[a][b] = "o"
     for k in P:
         print(k)
 
 while spr == 0:
-    ruch_x(P)
-    if P[0][0] == 2 and P[0][1] == 2 and P[0][2] == 1 or P[1][0] == 2 and P[1][1] == 2 and P[1][2] == 2 or P[2][0] == 2 and P[2][1] == 2 and P[2][2] == 2 or P[0][0] == 2 and P[1][0] == 2 and P[2][0] == 2 or P[0][1] == 2 and P[1][1] == 2 and P[2][1] == 2 or P[0][2] == 2 and P[1][2] == 2 and P[2][2] == 2 or P[0][0] == 2 and P[1][1] == 2 and P[2][2] == 2 or P[0][2] == 2 and P[1][1] == 2 and P[2][0] == 2:
+    ruch_o(P)
+    if P[0][0] == "o" and P[0][1] == "o" and P[0][2] == "o" or P[1][0] == "o" and P[1][1] == "o" and P[1][2] == "o" or P[2][0] == "o" and P[2][1] == "o" and P[2][2] == "o" or P[0][0] == "o" and P[1][0] == "o" and P[2][0] == "o" or P[0][1] == "o" and P[1][1] == "o" and P[2][1] == "o" or P[0][2] == "o" and P[1][2] == "o" and P[2][2] == "o" or P[0][0] == "o" and P[1][1] == "o" and P[2][2] == "o" or P[0][2] == "o" and P[1][1] == "o" and P[2][0] == "o":
         spr = 1
         print("wygrywa kółko")
-    ruch_o(P)
-    if P[0][0] == 1 and P[0][1] == 1 and P[0][2] == 1 or P[1][0] == 1 and P[1][1] == 1 and P[1][2] == 1 or P[2][0] == 1 and P[2][1] == 1 and P[2][2] == 1 or P[0][0] == 1 and P[1][0] == 1 and P[2][0] == 1 or P[0][1] == 1 and P[1][1] == 1 and P[2][1] == 1 or P[0][2] == 1 and P[1][2] == 1 and P[2][2] == 1 or P[0][0] == 1 and P[1][1] == 1 and P[2][2] == 1 or P[0][2] == 1 and P[1][1] == 1 and P[2][0] == 1:
+        break
+    ruch_x(P)
+    if P[0][0] == "x" and P[0][1] == "x" and P[0][2] == "x" or P[1][0] == "x" and P[1][1] == "x" and P[1][2] == "x" or P[2][0] == "x" and P[2][1] == "x" and P[2][2] == "x" or P[0][0] == "x" and P[1][0] == "x" and P[2][0] == "x" or P[0][1] == "x" and P[1][1] == "x" and P[2][1] == "x" or P[0][2] == "x" and P[1][2] == "x" and P[2][2] == "x" or P[0][0] == "x" and P[1][1] == "x" and P[2][2] == "x" or P[0][2] == "x" and P[1][1] == "x" and P[2][0] == "x":
         spr = 1
         print("wygrywa krzyżyk")
-
